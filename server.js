c2b8ecaf966e3e40b5fd6ff609e51a649a7f98f3
color <colorofgaming2013@gmail.com>
2016-05-29 12:55:36 +0200

    test

--- server.js
+++ server.js
@@ -149,9 +149,4 @@ app.post('/userName',function(req,res){
 });
 
 //Server start
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
-
-app.listen(server_port, server_ip_address, function () {
-    console.log( "Listening on " + server_ip_address + ", server_port " + server_port )
-});
+app.listen(process.env.PORT || 8080);
